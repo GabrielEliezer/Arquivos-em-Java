822ebffa1fb6477f00a98a0369548f51da12d28f
Eliezer <g.eliezer.rodrigues@gmail.com>
2024-02-15 21:11:25 -0300

    uparchive

---
+++ Lista1 exercicios 08-02-2024/Exercicio 18/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner teste = new Scanner(System.in);
+        System.out.println("A formula é ax+b=0 -- Informe o Valor de 'a': ");
+        double valordea = teste.nextDouble();
+        double x;
+
+        System.out.println("A formula é ax+b=0 -- Informe o Valor de 'b': ");
+        double valordeb = teste.nextDouble();
+
+        double res;
+
+
+
+    }
+}
